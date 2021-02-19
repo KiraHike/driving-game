44e62385c5cde7ef0a7077f1e1f1a530e81ec134
Kira Hike <kirahikes@gmail.com>
2021-02-18 17:06:35 -0800

    #2 Create handle function that turns the car and updates direction property

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+var $car = document.querySelector('.car');
+
 var drive = {
   direction: 'east'
+};
+
+function turn(event) {
+  if (event.code === 'KeyW') {
+    $car.className = 'car north';
+    drive.direction = 'north';
+  } else if (event.code === 'KeyS') {
+    $car.className = 'car south';
+    drive.direction = 'south';
+  } else if (event.code === 'KeyA') {
+    $car.className = 'car west';
+    drive.direction = 'west';
+  } else if (event.code === 'KeyD') {
+    $car.className = 'car east';
+    drive.direction = 'east';
+  }
 }
 
-var $car = document.querySelector('#car');
+document.addEventListener('keydown', turn);
