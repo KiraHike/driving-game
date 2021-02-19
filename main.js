571f84ff4059b5fa6dc584384ec7cc76344bc3d5
Kira Hike <kirahikes@gmail.com>
2021-02-18 17:51:17 -0800

    #3 added x and y coordinates to data model and changed function name

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 var $car = document.querySelector('.car');
 
-var drive = {
-  direction: 'east'
+var car = {
+  direction: 'east',
+  xAxis: 90,
+  yAxis: 90
 };
 
-function turn(event) {
+function drive(event) {
   if (event.code === 'KeyW' || event.code === 'ArrowUp') {
     $car.className = 'car north';
-    drive.direction = 'north';
+    car.direction = 'north';
   } else if (event.code === 'KeyS' || event.code === 'ArrowDown') {
     $car.className = 'car south';
-    drive.direction = 'south';
+    car.direction = 'south';
   } else if (event.code === 'KeyA' || event.code === 'ArrowLeft') {
     $car.className = 'car west';
-    drive.direction = 'west';
+    car.direction = 'west';
   } else if (event.code === 'KeyD' || event.code === 'ArrowRight') {
     $car.className = 'car east';
-    drive.direction = 'east';
+    car.direction = 'east';
   }
 }
 
-document.addEventListener('keydown', turn);
+document.addEventListener('keydown', drive);
