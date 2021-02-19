d1bdb10277a7f788032e87aaf0f0d7292adee585
Kira Hike <kirahikes@gmail.com>
2021-02-18 17:14:44 -0800

    #2 added arrow keys in addition to WASD

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ var drive = {
 };
 
 function turn(event) {
-  if (event.code === 'KeyW') {
+  if (event.code === 'KeyW' || event.code === 'ArrowUp') {
     $car.className = 'car north';
     drive.direction = 'north';
-  } else if (event.code === 'KeyS') {
+  } else if (event.code === 'KeyS' || event.code === 'ArrowDown') {
     $car.className = 'car south';
     drive.direction = 'south';
-  } else if (event.code === 'KeyA') {
+  } else if (event.code === 'KeyA' || event.code === 'ArrowLeft') {
     $car.className = 'car west';
     drive.direction = 'west';
-  } else if (event.code === 'KeyD') {
+  } else if (event.code === 'KeyD' || event.code === 'ArrowRight') {
     $car.className = 'car east';
     drive.direction = 'east';
   }
